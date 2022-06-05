2eb3a87182b74369a60f1769bee1e9ad24332b75
Jonathan Prado <jpradoar@gmail.com>
2022-06-05 11:46:09 -0300

    update polaris and add scripts

---
+++ scripts/validate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+echo "Validate yaml files" 
+find . -iname *.yaml -exec ls -l {} + |awk {'print "./polaris/cmd/polaris audit --color --format pretty --audit-path", $9'}|sh
+
+echo -e "\n\n\n\n#-----------------------------------------------\n"
+
+echo "Validate remote repo" 
+git remote get-url origin --push|cut -d":" -f2|awk {'print "trivy repo github.com/"$1'}|sh
+
+echo -e "\n\n\n\n#-----------------------------------------------\n"
+
+echo "Validate docker images"
+docker images |grep -v IMAGE |awk {'print "trivy image --security-checks  vuln,config,secret --no-progress --severity MEDIUM,HIGH,CRITICAL  ", $3'}|sh
+
