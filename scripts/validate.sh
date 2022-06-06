0b927d487db34fdd8eb0f5bbd5e9fc06d77945f1
Jonathan Prado <jpradoar@gmail.com>
2022-06-06 07:59:32 -0300

    Add tfsec test

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -13,3 +13,9 @@ echo -e "\n\n\n\n#-----------------------------------------------\n"
 echo "Validate docker images"
 docker images |grep -v IMAGE |awk {'print "trivy image --security-checks  vuln,config,secret --no-progress --severity MEDIUM,HIGH,CRITICAL  ", $3'}|sh
 
+
+echo -e "\n\n\n\n#-----------------------------------------------\n"
+
+echo "Validate Terraform"
+docker run --rm -it -v "$(pwd):/src" aquasec/tfsec /src
+
